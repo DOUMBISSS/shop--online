61c158b90526048223e6c9fd4887548b0c215835
Doumbisss <doumbia77fode@gmail.com>
2022-07-16 23:20:56 +0000

    second

--- js/app.js
+++ js/app.js
@@ -348,6 +348,7 @@ let btnClose= document.querySelector('.btn--close');
 
 cartIcon.addEventListener('click',Open);
 btnClose.addEventListener('click',close);
+let counter = document.querySelector('.counter');
 
 function Open() {
     cartContainer.classList.add("OpenCart");
@@ -355,3 +356,92 @@ function Open() {
 function close (){
     cartContainer.classList.remove("OpenCart"); 
 }
+let newArt = document.querySelector('.cart--articles');
+let btnAdd= document.querySelectorAll('.btn--add');
+for (i = 0 ; i < btnAdd.length ; i++)
+btnAdd[i].addEventListener('click',addArticle);
+
+
+function addArticle () {
+    document.querySelector('.counter').innerText = parseInt(document.querySelector('.counter').innerText) + 1 ;
+    // newArticle();
+
+    
+//     document.querySelector('.cart--container').innerHTML = `<div class="cart--container--block">
+//     <div class="cart--container--header">
+//         <div class="cart--description">
+//             <i class="fa-solid fa-bag-shopping"></i>
+//             <p>3 articles</p>
+//         </div>
+//         <div class="btn--close">
+//             <i class="fa-solid fa-xmark"></i>
+//         </div>
+//     </div>
+
+//     <div class="cart--container--content">
+//         <div class="cart--articles">
+//             <div class="button-block">
+//                 <div class="handle--quantity">
+//                   <div class="minus"><h5>-</h5></div>
+//                   <div class="qty"><h5>1</h5></div>
+//                   <div class="plus"><h5>+</h5></div>
+//                 </div>
+//             </div>
+
+//             <div class="cart--articles--descriptions">
+//                 <div class="block--articles">
+//                     <div class="articles">
+//                         <img src="images/jupe.png" alt="">
+//                     </div>
+
+//                     <div class="articles--details">
+//                         <p class="names--articles">Jupe volante</p>
+//                         <p class="price--articles"> 2000 F</p>
+//                         <p class="quantity">3 unités</p>
+//                     </div>
+//                 </div>
+//             </div>
+
+//             <div class="btn--remove--article">
+//                 <i class="fa-solid fa-xmark"></i>
+//             </div>
+//         </div>
+//     </div>
+// </div>`
+// btnClose();
+
+
+
+
+// document.querySelector('.cart--container').innerHTML = 'Your cart is empty';
+}
+
+// function newArticle(){
+//     document.querySelector('.counter').innerHTML =`<div class="cart--articles">
+//     <div class="button-block">
+//         <div class="handle--quantity">
+//           <div class="minus"><h5>-</h5></div>
+//           <div class="qty"><h5>1</h5></div>
+//           <div class="plus"><h5>+</h5></div>
+//         </div>
+//     </div>
+
+//     <div class="cart--articles--descriptions">
+//         <div class="block--articles">
+//             <div class="articles">
+//                 <img src="images/jupe.png" alt="">
+//             </div>
+
+//             <div class="articles--details">
+//                 <p class="names--articles">Jupe volante</p>
+//                 <p class="price--articles"> 2000 F</p>
+//                 <p class="quantity">3 unités</p>
+//             </div>
+//         </div>
+//     </div>
+
+//     <div class="btn--remove--article">
+//         <i class="fa-solid fa-xmark"></i>
+//     </div>
+// </div>`
+// }
